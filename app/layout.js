3adf3a2497998f157e960e07b1b769c96fa7298e
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-08-18 20:56:44 -0600

    Layout

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,18 @@
-export default function RootLayout() {
+export default function RootLayout({ children }) {
   return (
     <html>
-      <head>
-        <title>My App</title>
-      </head>
-      <body>Mi web</body>
+      <body>
+        <h1>Navbar</h1>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+        </ul>
+        {children}
+      </body>
     </html>
   );
 }
