abed218ee3163e1956c3c8fd953fa274c0ab9b73
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-08-23 09:19:55 -0600

    Metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,10 @@
 import Navbar from "../components/Navbar";
 
+export const metadata = {
+  title: "Welcome to my site",
+  description: "My NEXT.js site",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html>
